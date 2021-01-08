8316b25f830f080166b3e167980eca4686820527
liyu.gong@oracle.com <liygong@ocasvs-vm-standard-e3-8-ad1-01.visionprivatesu.ocasdev01iad.oraclevcn.com>
2021-01-08 04:22:03 +0000

    add training starter script

--- train_rec_crnn_mv3large_neck96_len100.sh
+++ train_rec_crnn_mv3large_neck96_len100.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=mv3-small
+#SBATCH --job-name=crnn_mv3large_neck96_len100
 #SBATCH --gres=gpu:1
 #SBATCH --time=5-00:00:00
 #SBATCH --partition batch
 
 
-#source ~/opt/anaconda3/bin/activate
-#conda activate ppocr-gpu
+source ~/opt/anaconda3/bin/activate
+conda activate ppocr-gpu
 
 
 python3 tools/train.py -c configs/rec/train/crnn_mv3large_neck96_len100.yml
